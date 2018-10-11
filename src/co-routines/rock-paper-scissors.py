e408127ffaf017a3752e6969e674b37aebd6a47f
RafaelDeRezende <44053932+RafaelDeRezende@users.noreply.github.com>
2018-10-11 10:52:56 +0200

    Update rock-paper-scissors.py

--- src/co-routines/rock-paper-scissors.py
+++ src/co-routines/rock-paper-scissors.py
@@ -4,24 +4,25 @@ DRAW = 0
 WIN = 1
 LOSE = -1
 
-def rock_paper_scissors():
-    beats = {
+beats = {
         "rock": ["scissors"],
         "scissors": ["paper"],
         "paper": ["rock"]
-        }
+    }
+
+def rock_paper_scissors():
 
     winning_prob = { k: 1. / 3. for k in beats.keys() }
 
     while True:
-        r = random.random()
-
+        r = random()
+        print(winning_prob)
         for k in beats.keys():
             if r < winning_prob[k]:
                 played = k
                 outcome, other_played = yield played
                 break
-            r - winning_prob[k]
+            r -= winning_prob[k]
 
         if outcome == DRAW:
             pass
@@ -34,7 +35,21 @@ def rock_paper_scissors():
 
 
 player1 = rock_paper_scissors()
+player2 = rock_paper_scissors()
 
 a = next(player1)
-print(a)
-newa = player1.send((WIN, "rock")
+b = next(player2)
+
+for i in range(10):
+    print(a, b)
+    
+    if a in beats[b]:
+        result = WIN
+        a, b = player1.send((WIN, b)), player2.send((LOSE, a))
+    elif b in beats[a]:
+        result = LOSE
+        a, b = player1.send((LOSE, b)), player2.send((WIN, a))
+    else:
+        result = DRAW
+        a, b = player1.send((result, b)), player2.send((result, a))
+        
